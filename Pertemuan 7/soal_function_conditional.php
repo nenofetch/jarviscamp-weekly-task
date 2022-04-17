89b6d9382e8265e16e6661acca25f3b2016b19b6
Neno Arisma <nenoarisma@gmail.com>
2022-04-17 23:08:25 +0700

    update some task temp

--- Pertemuan 7/soal_function_conditional.php
+++ Pertemuan 7/soal_function_conditional.php
@@ -13,27 +13,30 @@
     <?php
 
 echo "<h3> Soal No 1 Greetings </h3>";
-/* 
+/*
 Soal No 1
 Greetings
-Buatlah sebuah function greetings() yang menerima satu parameter berupa string. 
+Buatlah sebuah function greetings() yang menerima satu parameter berupa string.
 
 contoh: greetings("abduh");
 Output: "Halo Abduh, Selamat Datang di Sanbercode!"
 */
 
 // Code function di sini
-
+function greetings($nama)
+{
+  echo "<h1>Halo $nama, Selamat Datang di SanberCode</h1>";
+}
 
 // Hapus komentar untuk menjalankan code!
-// greetings("Bagas");
-// greetings("Wahyu");
-// greetings("Abdul");
+greetings("Bagas");
+greetings("Wahyu");
+greetings("Abdul");
 
 echo "<br>";
 
 echo "<h3>Soal No 2 Reverse String</h3>";
-/* 
+/*
 Soal No 2
 Reverse String
 Buatlah sebuah function reverseString() untuk mengubah string berikut menjadi kebalikannya menggunakan function dan looping (for/while/do while).
@@ -45,24 +48,30 @@ Output: ludba
 
 */
 
-// Code function di sini 
-
+// Code function di sini
+function reverseString($nama) {
+  $panjang = strlen($nama);
+  for ($i= ($panjang - 1);  $i >= 0; $i--) {
+    echo "$nama[$i]";
+  }
+  echo "<br>";
+}
 
 // Hapus komentar di bawah ini untuk jalankan Code
-// reverseString("abduh");
-// reverseString("Sanbercode");
-// reverseString("We Are Sanbers Developers")
+reverseString("abduh");
+reverseString("Sanbercode");
+reverseString("We Are Sanbers Developers");
 echo "<br>";
 
 echo "<h3>Soal No 3 Palindrome </h3>";
-/* 
-Soal No 3 
+/*
+Soal No 3
 Palindrome
-Buatlah sebuah function yang menerima parameter berupa string yang mengecek apakah string tersebut sebuah palindrome atau bukan. 
+Buatlah sebuah function yang menerima parameter berupa string yang mengecek apakah string tersebut sebuah palindrome atau bukan.
 Palindrome adalah sebuah kata atau kalimat yang jika dibalik akan memberikan kata yang sama contohnya: katak, civic.
 Jika string tersebut palindrome maka akan mengembalikan nilai true, sedangkan jika bukan palindrome akan mengembalikan false.
-NB: 
-Contoh: 
+NB:
+Contoh:
 palindrome("katak") => output : "true"
 palindrome("jambu") => output : "false"
 NB: DILARANG menggunakan built-in function PHP seperti strrev() dll. Gunakan looping seperti biasa atau gunakan function reverseString dari jawaban no.2!
@@ -82,9 +91,9 @@ NB: DILARANG menggunakan built-in function PHP seperti strrev() dll. Gunakan loo
 echo "<h3>Soal No 4 Tentukan Nilai </h3>";
 /*
 Soal 4
-buatlah sebuah function bernama tentukan_nilai . Di dalam function tentukan_nilai yang menerima parameter 
-berupa integer. dengan ketentuan jika paramater integer lebih besar dari sama dengan 85 dan lebih kecil sama dengan 100 maka akan mereturn String “Sangat Baik” 
-Selain itu jika parameter integer lebih besar sama dengan 70 dan lebih kecil dari 85 maka akan mereturn string “Baik” selain itu jika parameter number lebih besar 
+buatlah sebuah function bernama tentukan_nilai . Di dalam function tentukan_nilai yang menerima parameter
+berupa integer. dengan ketentuan jika paramater integer lebih besar dari sama dengan 85 dan lebih kecil sama dengan 100 maka akan mereturn String “Sangat Baik”
+Selain itu jika parameter integer lebih besar sama dengan 70 dan lebih kecil dari 85 maka akan mereturn string “Baik” selain itu jika parameter number lebih besar
 sama dengan 60 dan lebih kecil dari 70 maka akan mereturn string “Cukup” selain itu maka akan mereturn string “Kurang”
 */
 
--- Pertemuan 7/soal_looping.php
+++ Pertemuan 7/soal_looping.php
@@ -11,15 +11,15 @@
 <body>
     <h1>Berlatih Looping</h1>
 
-    <?php 
+    <?php
         echo "<h3>Soal No 1 Looping I Love PHP</h3>";
-        /* 
-            Soal No 1 
+        /*
+            Soal No 1
             Looping I Love PHP
-            Lakukan Perulangan (boleh for/while/do while) sebanyak 20 iterasi. Looping terbagi menjadi dua: Looping yang pertama Ascending (meningkat) 
-            dan Looping yang ke dua menurun (Descending). 
+            Lakukan Perulangan (boleh for/while/do while) sebanyak 20 iterasi. Looping terbagi menjadi dua: Looping yang pertama Ascending (meningkat)
+            dan Looping yang ke dua menurun (Descending).
 
-            Output: 
+            Output:
             LOOPING PERTAMA
             2 - I Love PHP
             4 - I Love PHP
@@ -44,14 +44,20 @@
             2 - I Love PHP
         */
         // Lakukan Looping Di Sini
-
+        for($i = 2; $i <= 15; $i++) {
+          echo "$i - I Love PHP </br>";
+        }
+        echo "<p>Looping Kedua </p>";
+        for($i = 15; $i >= 2; $i--) {
+          echo "$i - I Love PHP </br>";
+        }
 
         echo "<h3>Soal No 2 Looping Array Modulo </h3>";
-        /* 
+        /*
             Soal No 2
             Looping Array Module
             Carilah tambah tiap anggka pada array dengan 5.
-            Tampung ke dalam array baru bernama $rest 
+            Tampung ke dalam array baru bernama $rest
         */
 
         $numbers = [18, 45, 29, 61, 47, 34];
@@ -60,46 +66,46 @@
         // Lakukan Looping di sini
 
         echo "<br>";
-        echo "Array tambah 5 adalah:  "; 
+        echo "Array tambah 5 adalah:  ";
         echo "<br>";
 
         echo "<h3> Soal No 3 Looping Asociative Array </h3>";
-        /* 
+        /*
             Soal No 3
             Loop Associative Array
-            Terdapat data items dalam bentuk array dimensi. Buatlah data tersebut ke dalam bentuk Array Asosiatif. 
-            Setiap item memiliki key yaitu : id, name, price, description, source. 
-            
-            Output: 
-            Array ( [id] => 001 [name] => Keyboard Logitek [price] => 60000 [description] => Keyboard yang mantap untuk kantoran [source] => logitek.jpeg ) 
-            Array ( [id] => 002 [name] => Keyboard MSI [price] => 300000 [description] => Keyboard gaming MSI mekanik [source] => msi.jpeg ) 
-            Array ( [id] => 003 [name] => Mouse Genius [price] => 50000 [description] => Mouse Genius biar lebih pinter [source] => genius.jpeg ) 
-            Array ( [id] => 004 [name] => Mouse Jerry [price] => 30000 [description] => Mouse yang disukai kucing [source] => jerry.jpeg ) 
+            Terdapat data items dalam bentuk array dimensi. Buatlah data tersebut ke dalam bentuk Array Asosiatif.
+            Setiap item memiliki key yaitu : id, name, price, description, source.
+
+            Output:
+            Array ( [id] => 001 [name] => Keyboard Logitek [price] => 60000 [description] => Keyboard yang mantap untuk kantoran [source] => logitek.jpeg )
+            Array ( [id] => 002 [name] => Keyboard MSI [price] => 300000 [description] => Keyboard gaming MSI mekanik [source] => msi.jpeg )
+            Array ( [id] => 003 [name] => Mouse Genius [price] => 50000 [description] => Mouse Genius biar lebih pinter [source] => genius.jpeg )
+            Array ( [id] => 004 [name] => Mouse Jerry [price] => 30000 [description] => Mouse yang disukai kucing [source] => jerry.jpeg )
 
         */
         $items = [
-            ['001', 'Keyboard Logitek', 60000, 'Keyboard yang mantap untuk kantoran', 'logitek.jpeg'], 
+            ['001', 'Keyboard Logitek', 60000, 'Keyboard yang mantap untuk kantoran', 'logitek.jpeg'],
             ['002', 'Keyboard MSI', 300000, 'Keyboard gaming MSI mekanik', 'msi.jpeg'],
             ['003', 'Mouse Genius', 50000, 'Mouse Genius biar lebih pinter', 'genius.jpeg'],
             ['004', 'Mouse Jerry', 30000, 'Mouse yang disukai kucing', 'jerry.jpeg']
         ];
-        
-        // Output: 
-        
+
+        // Output:
+
         echo "<h3>Soal No 4 Asterix </h3>";
-        /* 
+        /*
             Soal No 4
             Asterix 5x5
-            Tampilkan dengan looping dan echo agar menghasilkan kumpulan bintang dengan pola seperti berikut: 
-            Output: 
-            * 
-            * * 
-            * * * 
-            * * * * 
+            Tampilkan dengan looping dan echo agar menghasilkan kumpulan bintang dengan pola seperti berikut:
+            Output:
+            *
+            * *
+            * * *
+            * * * *
             * * * * *
         */
         echo "Asterix: ";
-        echo "<br>";        
+        echo "<br>";
     ?>
 
 </body>
