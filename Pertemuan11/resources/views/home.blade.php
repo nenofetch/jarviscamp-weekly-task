25ffe3ab984d87d02e47f3f349bede5503d76cdc
Neno Arisma <nenoarisma@gmail.com>
2022-05-14 18:23:51 +0700

    Update

--- Pertemuan11/resources/views/home.blade.php
+++ Pertemuan11/resources/views/home.blade.php
@@ -2,14 +2,28 @@
 <html lang="en">
 
 <head>
+    <title></title>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>About JarvisCamp</title>
 </head>
 
 <body>
-    <h1>Halaman home dari view diakses dari Controller</h1>
+    <h1>JarvisCamp</h1>
+    <h2>Social Media Developer Santai Berkualitas</h2>
+    <p>Belajar dan Berbagi agar hidup ini semakin santai berkualitas</p>
+    <h3>Benefit Join di JarvisCamp</h3>
+    <ul>
+        <li>Mendapatkan motivasi dari sesama developer</li>
+        <li>Sharing knowledge dari para mastah Jarvis</li>
+        <li>Dibuat oleh calon web developer terbaik</li>
+    </ul>
+    <h3>Cara Bergabung ke JarvisCamp</h3>
+    <ol>
+        <li>Mengunjungi Website ini</li>
+        <li>Mendaftar di <a href="/register">Form Sign Up</a></li>
+        <li>Selesai!</li>
+    </ol>
 </body>
 
 </html>
--- Pertemuan11/resources/views/welcome.blade.php
+++ Pertemuan11/resources/views/welcome.blade.php
@@ -3,16 +3,14 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Welcome</title>
 </head>
 
 <body>
-    <h1>Selamat datang di</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi sunt modi incidunt fugit commodi. Dolores
-        eveniet reiciendis dolor earum aperiam, repellendus possimus ad obcaecati facere placeat, culpa nobis odio
-        cupiditate?</p>
+    <h1>SELAMAT DATANG</h1>
+    <h2>Terima Kasih telah bergabung di SanberBook. Social Media kita bersama!</h2>
 </body>
 
 </html>
