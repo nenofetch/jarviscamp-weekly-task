81aae596b637fb5b9a684c69a84032b6548706f5
Neno Arisma <nenoarisma@gmail.com>
2022-05-14 12:35:26 +0700

    initial laravel project

---
+++ Pertemuan11/app/Http/Controllers/AuthController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    function register()
+    {
+        return view("register");
+    }
+
+    function welcome()
+    {
+        return view("welcome");
+    }
+}
