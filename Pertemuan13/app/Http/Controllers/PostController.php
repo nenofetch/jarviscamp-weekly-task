d1b6c6116424908a7277c9c02eb0a9f510972705
Neno Arisma <nenoarisma@gmail.com>
2022-05-23 22:42:54 +0700

    Skeleton Pertemuan 13

---
+++ Pertemuan13/app/Http/Controllers/PostController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $result = Post::all();
+        return view('index', ["posts" => $result]);
+    }
+
+    public function create()
+    {
+        return view('create');
+    }
+
+    public function store(Request $request)
+    {
+        $data = [
+            "title" => $request->title,
+            "body" => $request->body
+        ];
+
+        $create = Post::create($data);
+
+        if ($create) {
+            return redirect('/');
+        }
+    }
+
+    public function edit($id)
+    {
+        $post = Post::find($id);
+        return view('edit', ['post' => $post]);
+    }
+    
+
+    public function show($id)
+    {
+        $result = Post::find($id);
+
+        return view('detail', ['post' => $result]);
+    }
+
+    public function destroy($id)
+    {
+        Post::destroy($id);
+        return redirect('/');
+    }
+}
